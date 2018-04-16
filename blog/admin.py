9690960a49d8ab0eefc6d45aae3936bfeaaebd44
Rafriki1963 <Rafael.martinez.munoz1991@gmail.com>
2018-04-16 12:16:49 +0200

    16/04/2018

---
+++ blog/admin.py
@@ -0,0 +1,5 @@
+# NEW 16/04/2018
+from django.contrib import admin
+from .models import Post
+
+admin.site.register(Post)
---
+++ blog/models.py
@@ -0,0 +1,21 @@
+# NEW 16/04/2018
+from django.db import models
+from django.utils import timezone
+
+# Clase Post, que contiene autor, titulo,...
+# todo hecho en un controlador de mysql
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
